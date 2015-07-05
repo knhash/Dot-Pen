4e70870d92a0aa320aabfcc0a3e7e16800be5a9e
Shashank S <mr.knhash@gmail.com>
2015-07-05 20:56:38 +0530

    Click Counter
    An android application that helps you count and keep track of any and
    everything

---
+++ app/src/main/java/knhash/track7/MainActivity.java
@@ -0,0 +1,158 @@
+package knhash.track7;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.melnykov.fab.FloatingActionButton;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final int ACTIVITY_CREATE=0;
+    private static final int ACTIVITY_EDIT=1;
+
+    private static final int INSERT_ID = Menu.FIRST;
+    private static final int DELETE_ID = Menu.FIRST + 1;
+    private static final int EDIT_ID = Menu.FIRST + 2;
+
+    private NotesDbAdapter mDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDbHelper = new NotesDbAdapter(this);
+        mDbHelper.open();
+        final ListView listview = (ListView) findViewById(R.id.list);
+        fillData(listview);
+        registerForContextMenu(listview);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                createNote();
+            }
+
+        });
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Intent i = new Intent(MainActivity.this, NoteEdit.class);
+                i.putExtra(NotesDbAdapter.KEY_ROWID, id);
+                startActivityForResult(i, ACTIVITY_EDIT);
+
+                /*//View v = (View)view.getParent();
+                TextView txt2 = (TextView) view.findViewById(R.id.text2);
+                String s = txt2.getText().toString();
+                int count = Integer.parseInt(s);
+                count++;
+                txt2.setText(String.valueOf(count));
+
+                txt2.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);*/
+            }
+        });
+
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                mDbHelper.deleteNote(id);
+                ListView listview = (ListView) findViewById(R.id.list);
+                fillData(listview);
+                return true;
+            }
+        });
+    }
+
+    private void fillData(ListView listview) {
+        // Get all of the rows from the database and create the item list
+        Cursor mNotesCursor = mDbHelper.fetchAllNotes();
+        startManagingCursor(mNotesCursor);
+
+        // Create an array to specify the fields we want to display in the list (only TITLE)
+        String[] from = new String[]{NotesDbAdapter.KEY_TITLE};
+
+        // and an array of the fields we want to bind those fields to (in this case just text1)
+        int[] to = new int[]{R.id.text1};
+
+        // Now create a simple cursor adapter and set it to display
+        SimpleCursorAdapter notes =
+                new SimpleCursorAdapter(this, R.layout.notes_row, mNotesCursor, from, to);
+        listview.setAdapter(notes);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.add(0, INSERT_ID, 0, R.string.menu_insert);
+        return true;
+    }
+
+    /*@Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+        menu.add(1, EDIT_ID, 1, R.string.menu_edit);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case DELETE_ID:
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                mDbHelper.deleteNote(info.id);
+                ListView listview = (ListView) findViewById(R.id.list);
+                fillData(listview);
+                return true;
+            case EDIT_ID:
+                info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                Intent i = new Intent(MainActivity.this, NoteEdit.class);
+                i.putExtra(NotesDbAdapter.KEY_ROWID, info.id);
+                startActivityForResult(i, ACTIVITY_EDIT);
+        }
+        return super.onContextItemSelected(item);
+    }*/
+
+    private void createNote() {
+        Intent i = new Intent(this, NoteEdit.class);
+        startActivityForResult(i, ACTIVITY_CREATE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        ListView listview = (ListView) findViewById(R.id.list);
+        fillData(listview);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        if (id == INSERT_ID) {
+            createNote();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/knhash/track7/NoteEdit.java
@@ -0,0 +1,183 @@
+package knhash.track7;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.HapticFeedbackConstants;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.melnykov.fab.FloatingActionButton;
+
+/**
+ * Created by Hash on 11-04-2015.
+ */
+public class NoteEdit extends ActionBarActivity {
+
+    private NotesDbAdapter mDbHelper;
+    private EditText mTitleText;
+    private EditText mBodyText;
+    private Long mRowId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDbHelper = new NotesDbAdapter(this);
+
+        //Prevent Keyboard on start
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        mDbHelper.open();
+        setContentView(R.layout.note_edit);
+
+        mTitleText = (EditText) findViewById(R.id.title);
+        mBodyText = (EditText) findViewById(R.id.body);
+
+        //Button confirmButton = (Button) findViewById(R.id.confirm);
+        FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.plus);
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.minus);
+        FloatingActionButton fab3 = (FloatingActionButton) findViewById(R.id.zero);
+
+        mRowId = (savedInstanceState == null) ? null :
+                (Long) savedInstanceState.getSerializable(NotesDbAdapter.KEY_ROWID);
+        if (mRowId == null) {
+            Bundle extras = getIntent().getExtras();
+            mRowId = extras != null ? extras.getLong(NotesDbAdapter.KEY_ROWID)
+                    : null;
+        }
+
+        populateFields();
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                String s = mBodyText.getText().toString();
+                int count = Integer.parseInt(s);
+                count--;
+                mBodyText.setText(String.valueOf(count));
+                setResult(RESULT_OK);
+                //finish();
+            }
+
+        });
+
+        fab1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                String s = mBodyText.getText().toString();
+                int count = Integer.parseInt(s);
+                count++;
+                mBodyText.setText(String.valueOf(count));
+                setResult(RESULT_OK);
+                //finish();
+            }
+
+        });
+
+        fab3.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                String s = mBodyText.getText().toString();
+                int count = 0;
+                mBodyText.setText(String.valueOf(count));
+                setResult(RESULT_OK);
+                //finish();
+            }
+
+        });
+
+        /*confirmButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                String s = mBodyText.getText().toString();
+                int count = Integer.parseInt(s);
+                count++;
+                mBodyText.setText(String.valueOf(count));
+                setResult(RESULT_OK);
+                //finish();
+            }
+
+        });*/
+    }
+
+    private void populateFields() {
+        if (mRowId != null) {
+            Cursor note = mDbHelper.fetchNote(mRowId);
+            startManagingCursor(note);
+            mTitleText.setText(note.getString(
+                    note.getColumnIndexOrThrow(NotesDbAdapter.KEY_TITLE)));
+            mBodyText.setText(note.getString(
+                    note.getColumnIndexOrThrow(NotesDbAdapter.KEY_BODY)));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        saveState();
+        outState.putSerializable(NotesDbAdapter.KEY_ROWID, mRowId);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveState();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        populateFields();
+    }
+
+    private void saveState() {
+        String title = mTitleText.getText().toString();
+        String body = mBodyText.getText().toString();
+
+        if (mRowId == null && !title.equals("")) {
+            long id = mDbHelper.createNote(title, body);
+            if (id > 0) {
+                mRowId = id;
+            }
+        }
+
+        else if (title.equals("")){
+            return;
+        }
+
+        else {
+            mDbHelper.updateNote(mRowId, title, body);
+        }
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        int keyCode = event.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    String s = mBodyText.getText().toString();
+                    int count = Integer.parseInt(s);
+                    count++;
+                    mBodyText.setText(String.valueOf(count));
+                    setResult(RESULT_OK);
+                }
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    String s = mBodyText.getText().toString();
+                    int count = Integer.parseInt(s);
+                    count--;
+                    mBodyText.setText(String.valueOf(count));
+                    setResult(RESULT_OK);
+                }
+                return true;
+            default:
+                return super.dispatchKeyEvent(event);
+        }
+    }
+}
